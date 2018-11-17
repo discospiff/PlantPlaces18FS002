ddcfd570139250cc01db791601f35c078ee9020d
jonesbr <brandan.jones@uc.edu>
2018-11-17 10:34:25 -0500

    Save the uploaded file.

--- PlantPlaces18FS002/WcfService1/ReadXML.aspx.cs
+++ PlantPlaces18FS002/WcfService1/ReadXML.aspx.cs
@@ -9,14 +9,37 @@ namespace WcfService1
 {
     public partial class ReadXML : System.Web.UI.Page
     {
+        /// <summary>
+        /// Handle uploading XML on page load.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
             if (IsPostBack)
             {
                 // start our XML validation.
-                String[] allowedExtensions = { ".xml" };
-                string fileName =  XMLFileUpload.FileName;
-                string fileExtension = System.IO.Path.GetExtension(fileName).ToLower();
+                String allowedExtension = ".xml" ;
+                if (XMLFileUpload.HasFile)
+                {
+                    string fileName = XMLFileUpload.FileName;
+                    string fileExtension = System.IO.Path.GetExtension(fileName).ToLower();
+
+                    // is this an allowed xml file?
+                    if (fileExtension == allowedExtension)
+                    {
+                        // construct the path where I want to save the file.
+                        String path = Server.MapPath("~/XML/");
+
+                        // save our XML file.
+                        XMLFileUpload.PostedFile.SaveAs(path + XMLFileUpload.FileName);
+
+                        LblXMLValidation.Text = "File Uploaded";
+                    } else
+                    {
+                        LblXMLValidation.Text = "File extension not permitted.";
+                    }
+                }
             }
         }
 
