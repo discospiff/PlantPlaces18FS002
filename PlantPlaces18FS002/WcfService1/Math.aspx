a4a6fca87a519726b18e5055c2f585715051524b
jonesbr <brandan.jones@uc.edu>
2018-12-01 11:55:46 -0500

    Consume a web service

---
+++ PlantPlaces18FS002/WcfService1/Math.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Math.aspx.cs" Inherits="WcfService1.Math" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Number 1:"></asp:Label>
+            <asp:TextBox ID="Number1" runat="server"></asp:TextBox>
+            <asp:Label ID="Label2" runat="server" Text="Number 2:"></asp:Label>
+            <asp:TextBox ID="Number2" runat="server"></asp:TextBox>
+            <asp:Button ID="BtnSum" runat="server" OnClick="BtnSum_Click" Text="=" />
+            <asp:Label ID="LblSum" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PlantPlaces18FS002/WcfService1/Math.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WcfService1
+{
+    public partial class Math : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSum_Click(object sender, EventArgs e)
+        {
+            Calculator.CalculatorSoapClient calcualtor = new Calculator.CalculatorSoapClient("CalculatorSoap");
+            string number1 = Number1.Text;
+            int intNumber1 = Convert.ToInt32(number1);
+            string number2 = Number2.Text;
+            int intNumber2 = Convert.ToInt32(number2);
+
+            int sum = calcualtor.Add(intNumber1, intNumber2);
+
+            LblSum.Text = "" + sum;
+        }
+    }
+}
